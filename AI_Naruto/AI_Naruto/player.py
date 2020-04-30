458dbf62163515d996c15ca4a8eca49376d5031d
NICK.yao <790536267@qq.com>
2020-04-30 22:48:06 +1000

    update component

--- AI_Naruto/AI_Naruto/player.py
+++ AI_Naruto/AI_Naruto/player.py
@@ -16,18 +16,38 @@ BLACK_INITIAL_SQUARES = [(0, 7), (1, 7), (3, 7), (4, 7), (6, 7), (7, 7),
 WHITE_INITIAL_SQUARES = [(0, 1), (1, 1), (3, 1), (4, 1), (6, 1), (7, 1),
                          (0,0), (1,0), (3,0), (4,0), (6,0), (7,0)]
 
+MAX_DEPTH = 5  # the maximum depth that minimax algorithm explores
+
 def _NEAR_SQUARES(square):
     x, y = square
     return {(x-1,y+1),(x,y+1),(x+1,y+1),
             (x-1,y),          (x+1,y),
             (x-1,y-1),(x,y-1),(x+1,y-1)} & ALL_SQUARES
 
+def heuristic(state):
+    """
+    Heuristic function. Return the minimum manhattan distance
+    between one of white token and one of the black token.
+    Return 0 when no black (enemy) tokens on board.
+    """
+    dist = 2147483647
+    if len(state.enemies) == 0:
+        return 0
+
+    for my_token in state.my_tokens.keys():
+        for enemy_token in state.enemies.keys():
+            xy1 = my_token
+            xy2 = enemy_token
+            dist = min(dist, abs(xy1[0] - xy2[0]) + abs(xy1[1] - xy2[1]))
+    return dist
+
 class Board:
 
-    def __init__(self):
+    def __init__(self, mycolor):
         self.board = Counter({xy:0 for xy in ALL_SQUARES})
-        self.curent_white_dict = {}
-        self.curent_black_dict = {}
+        self.curent_white_dict = dict()
+        self.curent_black_dict = dict()
+        self.mycolor = mycolor
 
         #白色用正数表示token数量，黑色用负数表示token数量
         for xy in WHITE_INITIAL_SQUARES:
@@ -39,6 +59,11 @@ class Board:
             self.curent_black_dict[xy] = 1
         self.score = {'white': 12, 'black': 12}
 
+        # from part A
+        edge = range(0, 8)
+        self.blocks = sorted({(q, r) for q in edge for r in edge})
+
+#update the board
     def update(self, color, action):
         atype, *aargs = action
         if atype == "MOVE":
@@ -73,11 +98,19 @@ class Board:
                     if self.board[near_square] != 0:
                         to_boom.append(near_square)
 
+    def __contains__(self, qr):
+        return qr in self.blocks
 
+# state
+# p, q, r => number of tokens in the stack,  x coodinate, y coordinate
 class State:
     """
     Game state. including a board, white (my_token) tokens and black (enemy) tokens.
     """
+    board = None
+    enemies = None
+    my_tokens = None
+
     def __init__(self, board, my_tokens, enemies):
 
         self.board = board
@@ -170,10 +203,16 @@ class State:
 
 
 
+
+
 class AI_NarutoPlayer:
 
-    max_depth = 5  # the maximum depth that minimax algorithm explores
+
     turns = 0  # current turn
+    color = None
+    opponent_color = None
+    board = None
+    state = None
 
 
     def __init__(self, colour):
@@ -190,11 +229,19 @@ class AI_NarutoPlayer:
         # TODO: Set up state representation.
 
         self.color = colour
-        self.board = Board()
+        self.board = Board(self.color)
         if(self.color == 'white'):
             self.opponent_color = 'black'
+            self.init_my_tokens = self.board.curent_white_dict.copy()
+            self.init_opponent_tokens = self.board.curent_black_dict.copy()
         else:
             self.opponent_color = 'white'
+            self.init_my_tokens = self.board.curent_black_dict.copy()
+            self.init_opponent_tokens = self.board.curent_white_dict.copy()
+
+        # initialise state
+        self.state = State(self.board, self.init_my_tokens, self.init_opponent_tokens)
+
 
 
 
@@ -233,37 +280,51 @@ class AI_NarutoPlayer:
 
         self.board.update(colour, action)
 
-    def get_heuristic(self):
-        return 1
+    def get_possible_moves(self, token):
+        possible_moves = []
+        for step_directions_q, step_directions_r in STEP_DIRECTIONS:
+            p = self.my_tokens.get(token)
+            q, r = token
+            for i in range(1, p + 1):
+                q_next = q + step_directions_q * i
+                r_next = r + step_directions_r * i
+                qr_next = q_next, r_next
+                if qr_next in self.board:
+                    if not self.enemy_occupied(qr_next):
+                        # move i tokens from qr to qr_next, the remaining number of token in (q, r) will be p-i
+                        possible_moves.append(("MOVE", (i, token, qr_next)))
+        # possible_moves.append(("BOOM", token))
+
+        return possible_moves
 
     def alphabeta(self, pos, current_depth, alpha, beta):
         # increase depth
         current_depth += 1
 
         # if max depth is reached
-        if current_depth == self.max_depth:
+        if current_depth == MAX_DEPTH:
             # apply evaluation function
             return self.get_heuristic()
 
         if current_depth % 2 == 0:
             # min player's turn
             # loop all enemy pieces of our player
-            remaining_pieces = [p for p in self.enemy_pieces() if p.alive]
-            for piece in remaining_pieces:
-                posible_moves = pieces.move()
+            remaining_pieces = self.enemies
+            for token in remaining_pieces:
+                posible_actions = self.get_possible_moves(token)
                 #check if the piece can move
-                if len(posible_moves) == 0:
+                if len(posible_actions) == 0:
                     continue
                 else:
-                    for new_pos in posible_moves:
-                        old_pos = piece.pos
+                    for new_pos in posible_actions:
+                        old_pos = token.pos
                         #alpha beta pruning
                         if alpha < beta:
                             # move the piece into the aim square
-                            eliminated_pieces = piece.makemove(new_pos)
+                            eliminated_pieces = token.makemove(new_pos)
                             current_heuristic = self.alphabeta(new_pos, current_depth, alpha, beta)
                             # undo move
-                            piece.undomove(old_pos, eliminated_pieces)
+                            token.undomove(old_pos, eliminated_pieces)
                             #update beta
                             if beta > current_heuristic:
                                 beta = current_heuristic
@@ -272,23 +333,24 @@ class AI_NarutoPlayer:
             #max player's turn
             #loop all friend pieces of our player
             remaing_pieces = [p for p in self.friend_pieces() if p.alive]
-            for piece in remaing_pieces:
-                possible_moves = piece.moves()
+            for token in remaing_pieces:
+                possible_moves = token.moves()
                 #check if this piece can move
                 if len(possible_moves) == 0:
                     continue
                 else:
                     for new_pos in possible_moves:
                         #record old_pos for undo
-                        old_pos = piece.pos
+                        old_pos = token.pos
                         #do alpha beta pruning
                         if alpha < beta:
                             #move the piece into the aim square
-                            eliminated_pieces = piece.makemove(new_pos)
+                            eliminated_pieces = token.makemove(new_pos)
                             current_heuristic = self.alphabeta(new_pos, current_depth, alpha, beta)
                             #undo move
-                            piece.undomove(old_pos, eliminated_pieces)
+                            token.undomove(old_pos, eliminated_pieces)
                             #update alpha
                             if alpha < current_heuristic:
                                 alpha = current_heuristic
             return alpha
+
